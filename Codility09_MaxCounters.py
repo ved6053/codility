47dd3e5c576769243fec3885a69ad5bb79d9a96e
ved6053 <ved.cse@gmail.com>
2024-05-26 15:10:47 +0530

    09 MaxCounters

---
+++ Codility09_MaxCounters.py
@@ -0,0 +1,70 @@
+'''
+MaxCounters
+You are given N counters, initially set to 0, and you have two possible operations on them:
+
+increase(X) − counter X is increased by 1,
+max counter − all counters are set to the maximum value of any counter.
+A non-empty array A of M integers is given. This array represents consecutive operations:
+
+if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+if A[K] = N + 1 then operation K is max counter.
+For example, given integer N = 5 and array A such that:
+
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+the values of the counters after each consecutive operation will be:
+
+    (0, 0, 1, 0, 0)
+    (0, 0, 1, 1, 0)
+    (0, 0, 1, 2, 0)
+    (2, 2, 2, 2, 2)
+    (3, 2, 2, 2, 2)
+    (3, 2, 2, 3, 2)
+    (3, 2, 2, 4, 2)
+The goal is to calculate the value of every counter after all operations.
+
+Write a function:
+
+class Solution { public int[] solution(int N, int[] A); }
+
+that, given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
+
+Result array should be returned as an array of integers.
+
+For example, given:
+
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+the function should return [3, 2, 2, 4, 2], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N and M are integers within the range [1..100,000];
+each element of array A is an integer within the range [1..N + 1].
+'''
+def max_counters(N, A):
+    res = [0 for _ in range(N)]
+    max_val = 0
+    min_val = 0
+    for ops in A: 
+        if N+1 == ops:
+            min_val = max_val
+        else:
+            ops-=1
+            res[ops] = max(res[ops]+1, min_val+1)
+            max_val = max(res[ops], max_val)
+    res = [max(ops, min_val) for ops in res]
+    return res
+
+if __name__=='__main__':
+    print(max_counters(N=5, A=[3, 4, 4, 6, 1, 4, 4]))
